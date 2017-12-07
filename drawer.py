02c9a37b3778e8cbb6dd994246ae7201de14a9c5
Lukáš Hejcman <lukas.hejcman@outlook.com>
2017-12-07 13:07:35 +0100

    Image exporter and iteration prompt.

--- drawer.py
+++ drawer.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
 
+import math
 import random
+import sys
+import time
 
 import pygame
+from PIL import Image
 from pygame import gfxdraw
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 700))
+win_width = 1000
+win_height = 800
+
+iterations_input = int(input("How many iterations to generate?"))
+
+screen = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("py-sierpinski-dice")
 
-x = {'1': 10, '2': 360, '3': 710}  # TODO: Automatically calculate the point positions
-y = {'1': 606, '2': 10, '3': 606}
+x = {'1': 10,
+     '2': int((win_width-100)/2+10),
+     '3': win_width-90}
+y = {'1': int(math.sqrt(((win_height+100)**2)-(int((win_width-100)/2+10))**2)),
+     '2': 10,
+     '3': int(math.sqrt(((win_height+100)**2)-(int((win_width-100)/2+10))**2))}
 
 red = (250, 0, 0)
 green = (0, 250, 0)
@@ -31,26 +44,39 @@ pygame.display.update()
 
 def drawer(gen_x, gen_y):
     iteration = 0
-    while True:
+    try:
+        while iteration != iterations_input:
 
-        iteration += 1
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    image_saver(iteration)
 
-        dice = str(random.randint(1, 3))
+            iteration += 1
 
-        gen_x = (gen_x + x[dice]) / 2
-        gen_y = (gen_y + y[dice]) / 2
+            dice = str(random.randint(1, 3))
 
-        label = font.render("Iteration: %s" % str(iteration), False, (255, 255, 255), (0, 0, 0))
-        screen.blit(label, (600, 50))
+            gen_x = (gen_x + x[dice]) / 2
+            gen_y = (gen_y + y[dice]) / 2
 
-        pygame.gfxdraw.pixel(screen, int(gen_x), int(gen_y), green)
-        pygame.display.update()
+            label = font.render("N. of points:%s" % str(iteration), False, (255, 255, 255), (0, 0, 0))
+            screen.blit(label, (win_width-200, 20))
 
-# TODO: Calculate the number of iterations etc.
+            pygame.gfxdraw.pixel(screen, int(gen_x), int(gen_y), green)
+            pygame.display.update()
+    finally:
+        time.sleep(10)
+        image_saver(iteration)
+
+
+def image_saver(num):
+    data = pygame.image.tostring(screen, 'RGBA')
+    img = Image.frombytes('RGBA', (win_width, win_height), data)
+    img.save('it_%s.png' % str(num), 'PNG')
+
+    pygame.quit()
+    sys.exit(0)
+
 
 if __name__ == '__main__':
     generated_points = drawer(rand_x, rand_y)
-        #for event in pygame.event.get():
-            #if event.type == pygame.QUIT:
-                #pygame.quit()
-                #sys.exit(0)
+
